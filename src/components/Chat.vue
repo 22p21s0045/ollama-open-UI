44f283957d2158934d6a89f1dc60f8a1efdcde23
22p21s0045 <monkeytitle@hotmail.com>
2024-04-09 08:38:56 +0700

    add: v0.0.1 success

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -1,4 +1,6 @@
 <script setup>
+import { computed } from "vue"
+
 const props = defineProps({
   history: {
     type: Array,
@@ -6,10 +8,14 @@ const props = defineProps({
     required: true,
   },
 })
+const histories = computed(()=>{
+  console.log("History recompute");
+  return props.history
+})
 </script>
 <template>
-  <div class="w-full h-full max-h-fit bg-red-400 flex flex-col gap-2 overflow-auto">
-    <div class="max-h-fit w-full bg-black flex flex-row" alt="container">
+  <div class="w-full h-full max-h-fit flex flex-col gap-2 overflow-auto">
+    <div class="max-h-fit w-full bg-black flex flex-row" alt="container" v-for="(history,index) in histories" :key="index">
       <div class="avatar p-3 pt-6 max-h-28">
         <div class="w-12 max-h-12 rounded">
           <img
@@ -19,7 +25,18 @@ const props = defineProps({
         </div>
       </div>
       <div class="p-7 text-justify max-h-fit">
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptates qui debitis enim sunt voluptatem beatae vel repellat soluta fuga pariatur? Quod error labore quidem inventore nam cupiditate ducimus harum ipsum minima rem veniam deleniti placeat sed, dignissimos debitis perferendis magnam veritatis officiis eius, quaerat assumenda quia asperiores aut. Veritatis, nam. Alias incidunt minima, corrupti consequuntur ratione fugit, omnis nisi perspiciatis error vel rem inventore aliquid dolorem assumenda ex ad. Inventore asperiores quaerat, quisquam quod eligendi optio quae pariatur nulla facilis sit labore quas vel, voluptas hic nihil et, iure exercitationem ut! Consectetur, libero quod consequuntur nobis enim omnis placeat debitis dolores atque magni. Molestias perferendis, rerum vero inventore corrupti obcaecati id est dolorum aliquid, illum porro? Perspiciatis officiis nisi, officia modi tempore eaque alias minima ipsam, sit veritatis cum eius qui corrupti repellat sunt architecto nam quam possimus. Adipisci, eligendi odio mollitia sit ab iusto animi natus provident atque distinctio aliquam in unde, odit excepturi rerum quaerat fugit repellendus hic quibusdam ipsa assumenda? Exercitationem sed voluptatibus doloribus numquam incidunt at deserunt cupiditate unde dolor inventore illo tempore ea repellat tenetur atque quaerat enim alias, fugit ab, explicabo molestiae nostrum deleniti. Saepe soluta tempora dolor eius excepturi omnis nobis rerum eos.
+        <div>
+{{history.request.content}}
+        </div>
+        <div v-if="history.response.content === ''">
+          <span class="loading loading-ring loading-sm"></span>
+        </div>
+        <div v-else>
+          
+
+          {{history.response.content}}
+        </div>
+        
       </div>
     </div>
     
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,8 +4,19 @@ import Chat from '../components/Chat.vue'
 import Layout from "@/components/Layout.vue";
 import Sidebar from "@/components/Sidebar.vue";
 import ChatVue from '@/components/Chat.vue';
+import { ChatManager } from '../../lib/ChatManager';
+import { reactive, ref, toRef, watch } from 'vue';
+const manager = reactive(new ChatManager())
+const historys = toRef(manager, 'historys')
+
+// watch( historys ,(newHistory)=>{
+//   console.log("Watch execute");
+//   console.log(newHistory)
+//   console.log(historys.value);
+// },{deep:true})
 const handleSentMessage = (value)=>{
   console.log(value + "Parent receive yet");
+  manager.sentChat(value)
 }
 </script>
 
@@ -15,7 +26,7 @@ const handleSentMessage = (value)=>{
       <Sidebar />
     </template>
     <template #chat>
-      <Chat/>
+      <Chat :history="historys"/>
     </template>
     <template #input-chat>
       <InputChat @sent-message = "handleSentMessage"/>
