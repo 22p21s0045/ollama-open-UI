ef0d13592790d495e61275e87fb799e9943b78f3
22p21s0045 <monkeytitle@hotmail.com>
2024-04-10 00:22:39 +0700

    add: v0.0.2

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -1,6 +1,19 @@
 <script setup>
 import { computed } from "vue"
+import { Marked } from "marked";
+import { markedHighlight } from "marked-highlight";
+import hljs from 'highlight.js';
 
+
+const marked = new Marked(
+  markedHighlight({
+    langPrefix: 'hljs language-',
+    highlight(code, lang, info) {
+      const language = hljs.getLanguage(lang) ? lang : 'plaintext';
+      return hljs.highlight(code, { language }).value;
+    }
+  })
+);
 const props = defineProps({
   history: {
     type: Array,
@@ -8,14 +21,19 @@ const props = defineProps({
     required: true,
   },
 })
-const histories = computed(()=>{
-  console.log("History recompute");
+const histories = computed(() => {
+  console.log("History recompute")
   return props.history
 })
 </script>
 <template>
   <div class="w-full h-full max-h-fit flex flex-col gap-2 overflow-auto">
-    <div class="max-h-fit w-full bg-black flex flex-row" alt="container" v-for="(history,index) in histories" :key="index">
+    <div
+      class="max-h-fit w-full bg-black flex flex-row"
+      alt="container"
+      v-for="(history, index) in histories"
+      :key="index"
+    >
       <div class="avatar p-3 pt-6 max-h-28">
         <div class="w-12 max-h-12 rounded">
           <img
@@ -26,22 +44,20 @@ const histories = computed(()=>{
       </div>
       <div class="p-7 text-justify max-h-fit">
         <div>
-{{history.request.content}}
+          {{ history.request.content }}
         </div>
         <div v-if="history.response.content === ''">
           <span class="loading loading-ring loading-sm"></span>
         </div>
         <div v-else>
-          
-
-          {{history.response.content}}
+          <article class="prose">
+            <div v-html=" marked.parse(history.response.content)"></div>
+           
+            
+          </article>
         </div>
-        
       </div>
     </div>
-    
-    
-    
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -3,12 +3,19 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+
 import App from './App.vue'
 import router from './router'
+import hljs from 'highlight.js/lib/core';
+import javascript from 'highlight.js/lib/languages/javascript';
+import hljsVuePlugin from "@highlightjs/vue-plugin";
+
+hljs.registerLanguage('javascript', javascript);
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(hljsVuePlugin)
 
 app.mount('#app')
